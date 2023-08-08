656f0b279e47889ea1986f957ab29e0bfe3dd08e
Arjunan K <arjunank680620@gmail.com>
2023-08-08 23:58:59 +0530

    Add price input container inside rent modal

--- app/components/inputs/ImageUpload.tsx
+++ app/components/inputs/ImageUpload.tsx
@@ -1,13 +1,15 @@
 "use client";
 
-import { CldUploadWidget } from "next-cloudinary";
 import Image from "next/image";
 import { useCallback } from "react";
+import { CldUploadWidget } from "next-cloudinary";
 import { TbPhotoPlus } from "react-icons/tb";
 
 declare global {
   var cloudinary: any;
 }
+
+const uploadPreset = "poylmewq";
 
 interface ImageUploadProps {
   onChange: (value: string) => void;
@@ -23,17 +25,42 @@ const ImageUpload: React.FC<ImageUploadProps> = ({ onChange, value }) => {
   );
 
   return (
-    <CldUploadWidget onUpload={handleUpload} uploadPreset="" options={{
+    <CldUploadWidget
+      onUpload={handleUpload}
+      uploadPreset={uploadPreset}
+      options={{
         maxFiles: 1,
-
-    }}>
-        {({ open }) => {
-            return (
-                <div onClick={() => open?.()} className="relative cursor-pointer hover:opacity-70 transition border-dashed border-2 p-20 border-neutral-300 flex flex-row ">
-
-                </div>
-            )
-        }}
+      }}
+    >
+      {({ open }) => {
+        return (
+          <div
+            onClick={() => open?.()}
+            className="relative
+            cursor-pointer
+            hover:opacity-70
+            transition
+            border-dashed 
+            border-2 
+            p-20 
+            border-neutral-300
+            flex
+            flex-col
+            justify-center
+            items-center
+            gap-4
+            text-neutral-600"
+          >
+            <TbPhotoPlus size={50} />
+            <div className="font-semibold text-lg">Click to upload</div>
+            {value && (
+              <div className="absolute inset-0 w-full h-full">
+                <Image alt="upload" fill className="object-cover" src={value} />
+              </div>
+            )}
+          </div>
+        );
+      }}
     </CldUploadWidget>
   );
 };
